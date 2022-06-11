a9edb08a2713ba19337397e3d51505f84d935e83
Ali Dehkhodaei <70447283+Alidehkhodaei@users.noreply.github.com>
2022-06-11 17:55:30 +0430

    Create 06.Logical operators.py

---
+++ 06.Logical operators.py
@@ -0,0 +1,14 @@
+number=2
+number2=4
+
+# and 
+if number==2 and number2>2:
+    print("Write some thing...")
+
+# or
+if number==2 or number2>2:
+    print("Write some thing...")
+
+# not
+if  not number2>2:
+    print("Write some thing...")
