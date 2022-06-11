10cb7e8a8149f0bbcb43e021b4eaf966ec62f092
Ali Dehkhodaei <70447283+Alidehkhodaei@users.noreply.github.com>
2022-06-11 17:52:46 +0430

    Create 01. Data Types And Operators.py

---
+++ 01. Data Types And Operators.py
@@ -0,0 +1,43 @@
+print('Data Types----------------')
+
+first_name="Ali" #String
+
+product_price=1000 #Integer
+
+is_recording=False #Boolean
+
+product_tags=["Python","Programming","What's python?"]  #List
+
+product={"first_name":"Ali","product_price":1000} #Dict
+
+name_person=None #Similar null in java
+
+print('------------------------')
+
+print(type(2))
+print(type(6.4))
+print(type(6j))
+print(type(first_name))
+print(type(product))
+print(type(is_recording))
+
+print('Operators----------------')
+
+print(4+6)
+print(7-2)
+print(4*6)
+print(3**2) #Power printed 9
+print(16**0.5) #Square printed 4.0
+print(15/2) #ِDivide always return float type printed 7.5
+print(15%2) #Divide remaining printed 1
+print(15//2) #Printed 7
+print(7+4/6*3)
+print((7+4)/6*3)
+
+print('Dynamic Type----------------')
+
+x=2
+x="Ali"
+x=3.6
+
+# C++ or Java are Static type
