11dc63b62f0f9d962b6ca15373aba59a84c8ba22
Ali Dehkhodaei <70447283+Alidehkhodaei@users.noreply.github.com>
2022-06-11 17:54:21 +0430

    Create 03.For.py

---
+++ 03.For.py
@@ -0,0 +1,21 @@
+# for
+
+# Collection => list of numbers, list of charcters in string, range(1,10), etc.
+
+list_Of_numbers=[1,2,3,4,5,6,7,8,9]
+
+for item in list_Of_numbers:
+ print(item)
+
+print("----------------")
+
+name="Ali"
+for i in name:
+ print(i)
+
+print("----------------")
+
+n=range(1,10)
+
+for i in n:
+ print(i)
