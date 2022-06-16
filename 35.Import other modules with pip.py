c0bbf65df99ac9a4e8215057319fc4f1b395ebc8
Ali Dehkhodaei <70447283+Alidehkhodaei@users.noreply.github.com>
2022-06-16 12:06:15 +0430

    Create 35.Import other modules with pip.py

---
+++ 35.Import other modules with pip.py
@@ -0,0 +1,17 @@
+# https://pypi.org
+
+#help(termcolor2) # Show items in termcolor2 module
+
+# Enter python / Enter help('modules') (Show modules list)
+
+import termcolor2 # For install (python3 -m pip install termcolor2 --user)
+
+print(termcolor2.colored("Ali Ahmadi",color="green"))
+
+print(termcolor2.colored("Ali Ahmadi",color="blue"))
+
+print(termcolor2.colored("Ali Ahmadi",color="red"))
+
+print(termcolor2.colored("Ali Ahmadi",color="yellow"))
+
+print(termcolor2.colored("Ali Ahmadi",color="white",on_color="on_magenta"))
