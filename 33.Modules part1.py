8e222f5da15d7dab2e5afaf128f92d698ccadd12
Ali Dehkhodaei <70447283+Alidehkhodaei@users.noreply.github.com>
2022-06-16 11:12:28 +0430

    Create 33.Modules part1.py

---
+++ 33.Modules part1.py
@@ -0,0 +1,15 @@
+# A module is a python file
+
+import random
+
+#import random as rand # A Custom name
+
+#from random import randint,choice # We can import functions where need.
+
+#from random import * #  import random
+
+#from random import randint as r_i,choice as r_ch
+
+print(random.randint(1,100))
+
+print(random.choice(["Ali","Reza","Hasan"]))
