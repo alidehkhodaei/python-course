a653427ea2859b80aa6cb9e27e50dbc30b8188ce
Ali Dehkhodaei <70447283+Alidehkhodaei@users.noreply.github.com>
2022-06-16 14:33:28 +0430

    Create 36-2.__name__.py

---
+++ 36-2.__name__.py
@@ -0,0 +1,12 @@
+def sayName2():
+    print(f"__name__ in second.py: {__name__}")
+
+
+def sayName3():
+    print(f"__name__ in second.py: {__name__}")
+
+
+if __name__=="__main__":
+    sayName2()  
+
+sayName3()
