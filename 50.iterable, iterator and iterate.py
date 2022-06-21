1f5ed448307b696eeb3d511914615624be57025a
Ali Dehkhodaei <70447283+alidehkhodaei@users.noreply.github.com>
2022-06-21 15:07:04 +0430

    Create 50.iterable, iterator and iterate.py

---
+++ 50.iterable, iterator and iterate.py
@@ -0,0 +1,20 @@
+# iterable
+# هر آبجکتی که قابلیت تبدیل شدن به ایتریتور رو دارد مثل لیست ها، ست ها و...
+
+# iterator
+# هر آبجکتی که می توانیم روی آیتم هاش پیمایش کنیم.
+
+# iterate
+# به هر پیمایش، iterate گفته میشود.
+
+items = [1, 2, 3, 4, 5] # iterable
+
+iter_items = iter(items) # iterator
+
+print(iter_items)  # <list_iterator object at 0x000001FAB05FA2F0>
+
+print(next(iter_items))
+print(next(iter_items))
+print(next(iter_items))
+print(next(iter_items))
+print(next(iter_items))
