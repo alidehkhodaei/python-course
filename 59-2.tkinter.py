dfd858b248a9c7d54e54432963e83578d5b844f5
Ali Dehkhodaei <70447283+alidehkhodaei@users.noreply.github.com>
2022-06-30 22:45:53 +0430

    Create 59-2.tkinter.py

---
+++ 59-2.tkinter.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.title("Ali")
+root.geometry("400x300")
+root.resizable(width=False, height=False)
+
+label=Label(root,text="Please enter your name:")
+label.place(x=0,y=0)
+
+input=Entry(root)
+input.place(x=0,y=20)
+
+def get_name():
+    print(input.get())
+
+btn=Button(root,text="Get name",command=lambda :get_name())
+btn.place(x=0,y=50)
+
+root.mainloop()
